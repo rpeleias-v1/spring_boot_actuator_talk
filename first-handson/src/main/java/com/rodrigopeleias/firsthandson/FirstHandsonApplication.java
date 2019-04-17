0a170db74e22c318a090e7514fd60b43c255a373
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:51:08 -0300

    Setup first project

---
+++ first-handson/src/main/java/com/rodrigopeleias/firsthandson/FirstHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.firsthandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstHandsonApplication.class, args);
+    }
+
+}
