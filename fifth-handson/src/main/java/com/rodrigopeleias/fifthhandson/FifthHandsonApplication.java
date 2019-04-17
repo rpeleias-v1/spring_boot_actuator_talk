e5c0053e4820e2f35fd90815c9ace9f308c52043
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:57:59 -0300

    Setup fifth project

---
+++ fifth-handson/src/main/java/com/rodrigopeleias/fifthhandson/FifthHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.fifthhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifthHandsonApplication.class, args);
+    }
+
+}
