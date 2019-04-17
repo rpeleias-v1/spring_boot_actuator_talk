6e2f0acf832f50f331aed66e2380a227d661b804
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:55:36 -0300

    Setup third project

---
+++ third-handson/src/main/java/com/rodrigopeleias/thirdhandson/ThirdHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.thirdhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHandsonApplication.class, args);
+    }
+
+}
---
+++ third-handson/src/test/java/com/rodrigopeleias/thirdhandson/ThirdHandsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigopeleias.thirdhandson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirdHandsonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
