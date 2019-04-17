d2e44548f7eb2de37e67bb1af5056bdcf139aa4e
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:59:07 -0300

    Setup sisth project

---
+++ sixth-handson/src/main/java/com/rodrigopeleias/sixthhandson/SixthHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.sixthhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixthHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixthHandsonApplication.class, args);
+    }
+
+}
---
+++ sixth-handson/src/test/java/com/rodrigopeleias/sixthhandson/SixthHandsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigopeleias.sixthhandson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SixthHandsonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
