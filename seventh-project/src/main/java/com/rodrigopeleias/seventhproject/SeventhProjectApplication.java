a9f1cd8a88f6ca19858f3fa9f7328978a5f07ec8
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 23:00:01 -0300

    Setup seventh project

---
+++ seventh-project/src/main/java/com/rodrigopeleias/seventhproject/SeventhProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.seventhproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeventhProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeventhProjectApplication.class, args);
+    }
+
+}
