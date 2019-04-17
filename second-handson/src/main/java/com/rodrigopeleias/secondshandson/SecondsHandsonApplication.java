a1fcb5e4a208f6d97889d7811606c48483915ac0
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:52:57 -0300

    Setup second project

---
+++ second-handson/src/main/java/com/rodrigopeleias/secondshandson/SecondsHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.secondshandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondsHandsonApplication.class, args);
+    }
+
+}
---
+++ second-handson/src/test/java/com/rodrigopeleias/secondshandson/SecondsHandsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigopeleias.secondshandson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecondsHandsonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
