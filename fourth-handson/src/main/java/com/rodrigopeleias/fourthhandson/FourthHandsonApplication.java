18cb334a2cae128baa4873fe61907e50bf0f6452
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-04-16 22:57:00 -0300

    Setup fourth project

---
+++ fourth-handson/src/main/java/com/rodrigopeleias/fourthhandson/FourthHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.fourthhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthHandsonApplication.class, args);
+    }
+
+}
---
+++ fourth-handson/src/test/java/com/rodrigopeleias/fourthhandson/FourthHandsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigopeleias.fourthhandson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FourthHandsonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
